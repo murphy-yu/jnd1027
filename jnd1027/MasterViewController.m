8547c1aa79dafb8641b4da2d59d16261c69d5f66
Murphy Yu <yuwj86@126.cpm>
2018-10-27 21:21:17 +0800

    5

--- jnd1027/MasterViewController.m
+++ jnd1027/MasterViewController.m
@@ -21,6 +21,8 @@
     // Do any additional setup after loading the view, typically from a nib.
     self.navigationItem.leftBarButtonItem = self.editButtonItem;
 
+    self.title = @"foo";
+    
     UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
     self.navigationItem.rightBarButtonItem = addButton;
     self.detailViewController = (DetailViewController *)[[self.splitViewController.viewControllers lastObject] topViewController];
